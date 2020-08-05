b3587faf70cdefbf44828a9ddee6065bb7ea625a
Chattarin Pardermwong <chattarinpar@mathregit.group>
2020-08-05 12:50:35 +0700

    step02

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,16 +1,42 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
 
     @GetMapping("/users")
-    public UsersResponse[] getAllUser(){
-        UsersResponse[] usersResponses = new UsersResponse[2];
-        usersResponses[0] = new UsersResponse(1, "User 1");
-        usersResponses[1] = new UsersResponse(2, "User 2");
-        return usersResponses;
+    public List<UsersResponse> getAllUser(@RequestParam(required = false, defaultValue ="1") int page,
+                                          @RequestParam(required = false, defaultValue = "10") int item_per_page){
+        List<UsersResponse> usersResponseList = new ArrayList<>();
+        usersResponseList.add(new UsersResponse(1,"user1"));
+        usersResponseList.add(new UsersResponse(2,"user2"));
+        usersResponseList.add(new UsersResponse(3,"user3"));
+        return usersResponseList;
+    }
+
+    @GetMapping("/queryparam")
+    public String getQueryParam(@RequestParam(required = false, defaultValue ="1") int page,
+                                @RequestParam(required = false, defaultValue = "10") int item_per_page){
+        return "page: " + page + " item: " + item_per_page;
+    }
+
+
+//    public UsersResponse[] getAllUser(){
+//        UsersResponse[] usersResponses = new UsersResponse[2];
+//        usersResponses[0] = new UsersResponse(1, "User 1");
+//        usersResponses[1] = new UsersResponse(2, "User 2");
+//        return usersResponses;
+//    }
+
+    @GetMapping("/users/{id}")
+    public UsersResponse getUserById(@PathVariable int id){
+        return new UsersResponse(id, "User " + id);
     }
 }
